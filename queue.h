91030796257a7dbf072a1887356bed21fb1f9988
Uğurcan Polat <ugurcan.polat12@gmail.com>
2018-11-22 11:55:28 +0300

    Implement queue data structure

---
+++ queue.h
@@ -0,0 +1,52 @@
+#include <linux/slab.h> // kmalloc()
+
+typedef struct string_t {
+    char* data;
+    String *next;
+} String;
+
+typedef struct queue_t {
+    String *front;
+    String *back;
+} Queue;
+
+String* newString(char* text) {
+    String *newData = (String*) kmalloc(sizeof(String));
+    newData->data = text;
+    newData->next = NULL;
+    return newData;
+}
+
+Queue* newQueue() {
+    Queue *q = (Queue*) kmalloc(sizeof(Queue));
+    q->front = q->back = NULL;
+    return q;
+}
+
+void enqueue(Queue* q, char* text) {
+    String* temp = newString(text);
+    
+    if (q->front == NULL) {
+        q->front = q->back = temp;
+        return;
+    }
+    
+    q->back->next = temp;
+    q->back = temp;
+}
+
+String* dequeue(Queue* q) {
+    if (q->front == NULL)
+        return NULL;
+    
+    String* dequeuedNode = q->front;
+    
+    if (q->front->next == NULL)
+        q->front = q->back = NULL;
+    else
+        q->front = q->front->next;
+
+    return temp;
+}
+
+
