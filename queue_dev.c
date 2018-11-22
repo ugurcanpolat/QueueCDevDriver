a66345e2b56f41110a4e56dd60ad5cfa5ab9e528
Uğurcan Polat <ugurcan.polat12@gmail.com>
2018-11-22 11:55:09 +0300

    Initial commit

---
+++ queue_dev.c
@@ -0,0 +1,40 @@
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/init.h>
+
+#include <linux/kernel.h>    /* printk() */
+#include <linux/slab.h>        /* kmalloc() */
+#include <linux/fs.h>        /* everything... */
+#include <linux/errno.h>    /* error codes */
+#include <linux/types.h>    /* size_t */
+#include <linux/proc_fs.h>
+#include <linux/fcntl.h>    /* O_ACCMODE */
+#include <linux/seq_file.h>
+#include <linux/cdev.h>
+
+#include <asm/switch_to.h>        /* cli(), *_flags */
+#include <asm/uaccess.h>    /* copy_*_user */
+
+#include "queue.h"
+#include "queue_ioctl.h"
+
+#define QUEUE_MAJOR 0
+#define QUEUE_NR_DEVS 4
+#define QUEUE_QUANTUM 4000
+#define QUEUE_QSET 1000
+
+int queue_major = QUEUE_MAJOR;
+int queue_minor = 0;
+int queue_nr_devs = QUEUE_NR_DEVS;
+int queue_quantum = QUEUE_QUANTUM;
+int queue_qset = QUEUE_QSET;
+
+module_param(queue_major, int, S_IRUGO);
+module_param(queue_minor, int, S_IRUGO);
+module_param(queue_nr_devs, int, S_IRUGO);
+module_param(queue_quantum, int, S_IRUGO);
+module_param(queue_qset, int, S_IRUGO);
+
+MODULE_AUTHOR("Musa Anıl Dogan, Ugurcan Polat, Umut Cem Avin");
+
+
